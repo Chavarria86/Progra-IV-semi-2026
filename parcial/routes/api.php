4839a577394104c328f449e1882fbef70ab44ebf
chavarria86 <USSS027724@ugb.edu.sv>
2026-05-02 19:06:37 -0600

    RAMA PARA EL PROYECTO FINAL

--- parcial/routes/api.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\Api\PasanteController;
-use App\Http\Controllers\Api\SupervisorController;
-
-Route::apiResource('pasantes', PasanteController::class);
-Route::apiResource('supervisores', SupervisorController::class);
