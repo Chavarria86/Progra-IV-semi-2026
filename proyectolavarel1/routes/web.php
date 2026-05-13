de4fb1607497407e59cbe80a5d46bad3b620b6dd
chavarria86 <USSS027724@ugb.edu.sv>
2026-05-13 10:52:12 -0600

    PROTOTIPO v1

--- proyectolavarel1/routes/web.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-use Illuminate\Support\Facades\Route;
-
-// SPA catch-all: todas las rutas web entregan la vista principal de Vue
-Route::get('/{any}', function () {
-    return view('app');
-})->where('any', '.*');
